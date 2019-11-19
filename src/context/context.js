cbdf742ebe3ca4efbbf9af8715a3de4e16a666dc
Jennie <cassiopeia5571@gmail.com>
2019-11-19 01:50:23 -0600

    context api added

--- src/context/context.js
+++ src/context/context.js
@@ -4,14 +4,46 @@ const ProductContext = React.createContext();
 //Provider
 //Consumer
 class ProductProvider extends Component {
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems:0
+  };
 
-    render() {
-        return (
-            <ProductContext.Provider value="hello from context">
-                {this.props.children}
-            </ProductContext.Provider>
-        );      
-    }
+  //handle sidebar
+  handleSideBar = () => {
+    this.setState({ sidebarOpen: !this.state.sidebarOpen });
+  };
+
+  //handle cart
+  handleCart = () => {
+    this.setState({ cartOpen: !this.state.cartOpen });
+  };
+
+  //close cart
+  closeCart = () => {
+    this.setState({ cartOpen: false });
+  };
+
+  //open
+  openCart = () => {
+    this.setState({ cartOpen: true });
+  };
+
+  render() {
+    return (
+      <ProductContext.Provider 
+      value={{
+          ...this.state,
+          handleSidebar:this.handleSidebar,
+          handleCart:this.handleCart,
+          closeCart:this.closeCart,
+          openCart: this.openCart
+      }}>
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
 }
 
 const ProductConsumer = ProductContext.Consumer;
