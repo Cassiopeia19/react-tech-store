37788a17321492f5445621f7ee8b9a280009c012
Jennie <cassiopeia5571@gmail.com>
2019-11-20 13:41:55 -0600

    sidebar added

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function SideCart() {
     return (
-        <>
+        <div>
             hello from side cart
-       </>
-    )
+       </div>
+    )        
 }
