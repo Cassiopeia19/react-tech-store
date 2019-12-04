9c068213d6402c46d85a81667fd4ef90bf25f0c2
Jennie <cassiopeia5571@gmail.com>
2019-12-04 15:52:56 -0600

    contact form

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+
 export default function Title({ title, center }) {
     return (
         <TitleWrapper className="row" center={center}>
