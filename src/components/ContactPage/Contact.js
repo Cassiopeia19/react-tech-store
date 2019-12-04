95ef6dd4336b786e6430a5220545aae15ba66108
Jennie <cassiopeia5571@gmail.com>
2019-12-04 16:54:36 -0600

    formspree

--- src/components/ContactPage/Contact.js
+++ src/components/ContactPage/Contact.js
@@ -7,26 +7,31 @@ export default function Contact() {
         <div className="row">
          <div className="col-10 mx-auto col-md-6 my-3">
              <Title title="contact us"/>
-         <form className="mt-5">
+                    <form className="mt-5" action="https://formspree.io/cassiopeia5571@gmail.com"
+                        method="POST">
             {/*first*/}
             <div className="form-group">
                 <input type="text" name="firstName"
-                className="form-control" placeholder="john smith"/>
+                className="form-control" placeholder="first name"/>
             </div>
             {/*email*/}
             <div className="form-group">
                 <input type="email" name="email"
-                    className="form-control" placeholder="email@email.com" />
+                    className="form-control" placeholder="emailaddress@email.com" />
             </div>
             {/*subject*/}
             <div className="form-group">
                 <input type="text" name="subject"
-                    className="form-control" placeholder="important!!!" />
+                    className="form-control" placeholder="subject" />
             </div>
             {/*message*/}
             <div className="form">
                 <textarea name="message" className="form-control"
-                 rows="10" placeholder="hello there buddy"/>
+                 rows="10" placeholder="your thoughts/concerns/suggestions"/>
+            </div>
+            {/*submit*/}
+            <div className="form-group mt-3">
+                <input type="submit" value="Send" className="form-control bg-primary text-white"/>
             </div>
              </form>    
          </div>
