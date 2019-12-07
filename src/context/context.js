dcfae7d71f9175d415642976cac0b5d18fa25c32
Jennie <cassiopeia5571@gmail.com>
2019-12-07 00:29:38 -0600

    footer added w/social media links

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {linkData} from './linkData';
+import { socialData } from './socialData';
 
 const ProductContext = React.createContext();
 //Provider
@@ -10,13 +11,14 @@ class ProductProvider extends Component {
     cartOpen: false,
     cartItems: 110,
     links: linkData,
+    socialIcons: socialData,
     cart:[]
   };
   // handle sidebar
   handleSidebar = () => {
     this.setState({ sidebarOpen: !this.state.sidebarOpen });
   };
-  // hanldle sart
+  // handle sart
   handleCart = () => {
     this.setState({ cartOpen: !this.state.sidebarOpen });
   };
